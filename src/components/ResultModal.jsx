20592be0222da9e0ea97190bb17cc92d56ee0b8b
Anish Bade <badeanish6045@gmail.com>
2024-05-18 15:47:04 +0545

    closing the modal via esc

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -14,7 +14,7 @@ const ResultModal = forwardRef(function ResultModal({ onReset, targetTime , rema
 
     });
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost  && <h2> You Lost </h2>}
       {!userLost && <h2> Your Score: {score} </h2>}
       <p>
